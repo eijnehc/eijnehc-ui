a1adeecefe199ded274c2de5f1c50561208dcafe
Chen Jie <e0036922@u.nus.edu>
2021-07-13 02:10:16 +0800

    Center layout

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,6 +13,7 @@ export const decorators = [
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  layout: 'centered',
   controls: {
     matchers: {
       color: /(background|color)$/i,
